4929e52d19ef210994f6cf37493536ea2922843a
Muhammad Diponegoro <66062420+DeuBoe19520@users.noreply.github.com>
2021-08-26 21:06:19 +0700

    Bikin Library PHP

---
+++ src/Customer.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Deubo\BelajarPhpLibrary;
+
+class Customer
+{
+
+    public function __construct(private string $name)
+    {
+    }
+
+    public function sayHello(string $name): string
+    {
+        return "Hello $name, my name is $this->name";
+    }
+
+}
